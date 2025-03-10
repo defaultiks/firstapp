3ce4327d4e4d5028d4d5b05b053354e5b13446c1
Konstantin <rtrt28109@gmail.com>
2025-03-10 23:57:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 
-let a = 2
-let b = 2
-console.log('Какая-то фраза')
-console.log(a+b)
+// let a = 2
+// let b = 2
+// console.log('Какая-то фраза')
+// console.log(a+b)
+
+
+// УРОК 2
+
+
+// let month = 10
+// let firstName = 'Konstantin'
+
+// console.log('Месяц рождения ' + month)
+// console.log('Добро пожаловать на курс ' + firstName)
+
+// alert('Сегодня 10.03.2025' )
+
+document.getElementById('one').textContent += 'Hello world'
+
+typeof undefined
+console.log(typeof undefined)
+typeof 0
+console.log(typeof 0)
+typeof 10n
+console.log(typeof 1110n)
+typeof true
+console.log(typeof false)
+typeof 'cat'
+console.log(typeof 'cat')
+typeof Symbol('id')
+console.log(typeof Symbol('id'))
+console.log(typeof null)
+
+
