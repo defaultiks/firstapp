c0164bf812c186ed2c307a862802435ff6b2795a
Konstantin <rtrt28109@gmail.com>
2025-03-11 23:24:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,20 +16,70 @@
 
 // alert('Сегодня 10.03.2025' )
 
-document.getElementById('one').textContent += 'Hello world'
+// document.getElementById('one').textContent += 'Hello world'
 
-typeof undefined
-console.log(typeof undefined)
-typeof 0
-console.log(typeof 0)
-typeof 10n
-console.log(typeof 1110n)
-typeof true
-console.log(typeof false)
-typeof 'cat'
-console.log(typeof 'cat')
-typeof Symbol('id')
-console.log(typeof Symbol('id'))
-console.log(typeof null)
+// typeof undefined
+// console.log(typeof undefined)
+// typeof 0
+// console.log(typeof 0)
+// typeof 10n
+// console.log(typeof 1110n)
+// typeof true
+// console.log(typeof false)
+// typeof 'cat'
+// console.log(typeof 'cat')
+// typeof Symbol('id')
+// console.log(typeof Symbol('id'))
+// console.log(typeof null)
 
 
+// УРОК 3
+
+// function sayHello(){
+//     console.log('Привет мир!')
+// }
+
+// sayHello()
+
+// function add(a,b){
+//     return a + b
+// }
+
+// let result = add(5,3)
+// console.log(result)
+
+// const muliply = (a,b) => {
+//     return a * b
+// }
+
+// console.log(muliply(2,4));
+
+
+
+
+
+// let btn = document.getElementById('check-in')
+// let inputIn = document.getElementById('text')
+
+
+
+
+
+// btn.onclick = function(){
+//     console.log('Da')
+//     console.log(inputIn.value)
+
+//     let e = +inputIn.value
+
+//     div.innerHTML = e 
+// }
+
+let div = document.getElementById('out')
+
+let myHappy = prompt('Введите любое число')
+if (myHappy < 100 ){
+    console.log(myHappy)
+}
+else{
+    div.innerHTML = myHappy
+}
