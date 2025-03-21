41a098391fed1d0b91037f1601adc8adb92f4fa6
Konstantin <rtrt28109@gmail.com>
2025-03-21 20:28:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -202,11 +202,53 @@
     
 // }
 
-let a
-let b = 1
-let c = 2
+// let a
+// let b = 1
+// let c = 2
 
-alert(b ?? "неизвестно");  
-alert(a ?? "неизвестно"); 
-alert(c ?? "неизвестно");  
+// alert(b ?? "неизвестно");  
+// alert(a ?? "неизвестно"); 
+// alert(c ?? "неизвестно");  
+
+
+// УРОК 7
+
+
+// let a = 5
+// let btn = document.getElementById('check-in')
+// let div = document.getElementById('out')
+
+// btn.onclick = function elem(){
+//     div.innerHTML = a
+// }
+
+
+function add() {
+    calculate((a, b) => a + b);
+}
+
+function subtract() {
+    calculate((a, b) => a - b);
+}
+
+function multiply() {
+    calculate((a, b) => a * b);
+}
+
+function divide() {
+    calculate((a, b) => {
+        if (b === 0) {
+            alert("Ошибка: Деление на ноль!");
+            return null;
+        }
+        return a / b;
+    });
+}
+function calculate(operation) {
+    const firstNumber = parseFloat(document.getElementById("firstNumber").value);
+    const secondNumber = parseFloat(document.getElementById("secondNumber").value);
+    
+    const result = operation(firstNumber, secondNumber);
+    document.getElementById("result").innerText = "Результат: " + result;
+}
 
